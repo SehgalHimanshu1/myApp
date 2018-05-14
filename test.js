ba47ac5dc348f19bfd81ed9d2662049224ce07b4
Himanshu <sehgal.himan@gmail.com>
2018-05-14 19:37:00 +0530

    Commit after completing the Chap3

--- test.js
+++
@@ -1,6 +0,0 @@
-/*console.log(test = require("./app_server/myFirstModule"));
-test.logg();
-test.setVal(12);
-test.logg();  */
-const path = require('path');
-console.log(path.join(__dirname, 'hey'));
